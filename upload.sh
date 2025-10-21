c8c75f9d1e9975a80b85761f1a58d6046b23be7b
AdiistheGoat <145622622+AdiistheGoat@users.noreply.github.com>
2025-10-20 19:53:50 -0500

    removed unnecessary files

--- upload.sh
+++
@@ -1,5 +0,0 @@
-docker buildx create --name mybuilder --use
-docker buildx inspect --bootstrap
-docker login -u adityagoyal333
-docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile -t adityagoyal333/rainyday:rainyday_img --push .
-
